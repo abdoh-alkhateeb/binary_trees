2fce893971bc086c372d8ea58c17989f9613fbe4
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-05-16 00:43:58 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_
-#define _BINARY_TREES_
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 /**
  * struct binary_tree_s - Binary tree node.
@@ -22,4 +22,4 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-#endif /* _BINARY_TREES_ */
+#endif /* _BINARY_TREES_H_ */
